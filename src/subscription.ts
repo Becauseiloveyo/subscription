0b2b0421d9e8123736a42323a8d4a8acd5c60e9a
Becauseiloveyo <1079458933@qq.com>
2026-07-22 11:04:32 +0800

    Sync GKD subscription source to v6

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,16 +1,18 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
-import { batchImportApps } from '@gkd-kit/tools';
+import appList from './appList';
 import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
+  id: 2026061301,
+  name: 'Becauseiloveyo 自用订阅',
+  version: 6,
+  author: 'Becauseiloveyo',
+  updateUrl:
+    'https://raw.githubusercontent.com/Becauseiloveyo/subscription/main/dist/gkd.json5',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/Becauseiloveyo/subscription',
   categories,
   globalGroups,
-  apps: await batchImportApps(`${import.meta.dirname}/apps`),
+  apps: appList,
 });
