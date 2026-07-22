3cffc441efd3ab0a5176bfd3a40d37a88f6ca95f
Becauseiloveyo <1079458933@qq.com>
2026-07-22 11:04:45 +0800

    Add GKD rule categories

--- src/categories.ts
+++ src/categories.ts
@@ -1,3 +1,28 @@
 import { defineGkdCategories } from '@gkd-kit/define';
 
-export default defineGkdCategories([]);
+export default defineGkdCategories([
+  {
+    key: 0,
+    name: '开屏广告',
+  },
+  {
+    key: 1,
+    name: '弹窗处理',
+    enable: false,
+  },
+  {
+    key: 2,
+    name: '更新提示',
+    enable: false,
+  },
+  {
+    key: 3,
+    name: '青少年模式',
+    enable: false,
+  },
+  {
+    key: 4,
+    name: '快捷确认',
+    enable: false,
+  },
+]);
