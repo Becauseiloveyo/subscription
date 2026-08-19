fe4ddc4566d41f32d45067e5d0b43e3e3defece2
Becauseiloveyo <1079458933@qq.com>
2026-08-19 12:12:12 +0800

    Expand GKD rule categories

--- src/categories.ts
+++ src/categories.ts
@@ -1,28 +1,17 @@
 import { defineGkdCategories } from '@gkd-kit/define';
 
 export default defineGkdCategories([
-  {
-    key: 0,
-    name: '开屏广告',
-  },
-  {
-    key: 1,
-    name: '弹窗处理',
-    enable: false,
-  },
-  {
-    key: 2,
-    name: '更新提示',
-    enable: false,
-  },
-  {
-    key: 3,
-    name: '青少年模式',
-    enable: false,
-  },
-  {
-    key: 4,
-    name: '快捷确认',
-    enable: false,
-  },
+  { key: 0, name: '开屏广告', enable: true },
+  { key: 1, name: '弹窗处理', enable: false },
+  { key: 2, name: '更新提示', enable: false },
+  { key: 3, name: '青少年模式', enable: false },
+  { key: 4, name: '快捷确认', enable: false },
+  { key: 5, name: '评价提示', enable: false },
+  { key: 6, name: '通知提示', enable: false },
+  { key: 7, name: '权限提示', enable: false },
+  { key: 8, name: '局部广告', enable: false },
+  { key: 9, name: '全屏广告', enable: false },
+  { key: 10, name: '分段广告', enable: false },
+  { key: 11, name: '功能类', enable: false },
+  { key: 12, name: '其他', enable: false },
 ]);
