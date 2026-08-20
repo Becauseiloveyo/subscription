bf65c36ea6c9e72aa94ae20cb53704a7a3e6721b
Becauseiloveyo <1079458933@qq.com>
2026-08-20 11:22:00 +0800

    Bump to v11 with JD Health fast splash rule

--- src/subscription.ts
+++ src/subscription.ts
@@ -2,12 +2,13 @@ import { defineGkdSubscription } from '@gkd-kit/define';
 import appList from './appList';
 import categories from './categories';
 import globalGroups from './globalGroups';
+import jdHealthApp from './jdHealthApp';
 import wechatApp from './wechatApp';
 
 export default defineGkdSubscription({
   id: 2026061301,
   name: 'Becauseiloveyo 自用订阅',
-  version: 10,
+  version: 11,
   author: 'Becauseiloveyo',
   updateUrl:
     'https://raw.githubusercontent.com/Becauseiloveyo/subscription/main/dist/gkd.json5',
@@ -15,5 +16,11 @@ export default defineGkdSubscription({
   supportUri: 'https://github.com/Becauseiloveyo/subscription',
   categories,
   globalGroups,
-  apps: [...appList.filter((app) => app.id !== 'com.tencent.mm'), wechatApp],
+  apps: [
+    ...appList.filter(
+      (app) => app.id !== 'com.tencent.mm' && app.id !== 'com.jd.jdhealth',
+    ),
+    wechatApp,
+    jdHealthApp,
+  ],
 });
