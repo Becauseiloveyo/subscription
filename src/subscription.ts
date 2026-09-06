e3d61b992a26baa41800d0b86adb10b29fa9ab04
Becauseiloveyo <1079458933@qq.com>
2026-09-06 12:17:27 +0800

    v13: use dedicated Chaoxing app rules

--- src/subscription.ts
+++ src/subscription.ts
@@ -2,13 +2,14 @@ import { defineGkdSubscription } from '@gkd-kit/define';
 import appList from './appList';
 import categories from './categories';
 import globalGroups from './globalGroups';
+import chaoxingApp from './chaoxingApp';
 import jdHealthApp from './jdHealthApp';
 import wechatApp from './wechatApp';
 
 export default defineGkdSubscription({
   id: 2026061301,
   name: 'Becauseiloveyo 自用订阅',
-  version: 12,
+  version: 13,
   author: 'Becauseiloveyo',
   updateUrl:
     'https://raw.githubusercontent.com/Becauseiloveyo/subscription/main/dist/gkd.json5',
@@ -18,9 +19,13 @@ export default defineGkdSubscription({
   globalGroups,
   apps: [
     ...appList.filter(
-      (app) => app.id !== 'com.tencent.mm' && app.id !== 'com.jd.jdhealth',
+      (app) =>
+        app.id !== 'com.tencent.mm' &&
+        app.id !== 'com.jd.jdhealth' &&
+        app.id !== 'com.chaoxing.mobile',
     ),
     wechatApp,
     jdHealthApp,
+    chaoxingApp,
   ],
 });
